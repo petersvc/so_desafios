27fafe07aa24c7e19f4858a0a96e88625acf4ec2
Peter Costa <petersvc@outlook.com>
2017-10-20 21:42:37 -0200

    syscall

---
+++ systemcall.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+  char print_1[40] = "Digite o nome do arquivo a ser lido: ";
+  int len = strlen(print_1);
+  char input_file_name[40];
+
+  write(0, print_1, len);
+  scanf("%[^\n]s", input_file_name);
+  getchar();
+
+  char print_2[40] = "Digite o nome do arquivo a ser gerado: ";
+  int len2 = strlen(print_2);
+  char output_file_name[40];
+
+  write(0, print_2, len2);
+  scanf("%[^\n]s", output_file_name);
+
+  return 0;
+}
---
+++ systemlight.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+
+  char input_file_name[128];
+
+  write(0, "Digite o nome do arquivo a ser lido: ", 38);
+  read(0, input_file_name, 128);
+  getchar();
+
+  char output_file_name[128];
+
+  write(0, "Digite o nome do arquivo a ser gerado: ", 40);
+  read(0, input_file_name, 128);
+
+  return 0;
+}
