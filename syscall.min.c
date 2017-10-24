a5ade5383cf3683f57098c54a7ca1f1eefc4612b
petersvc <petersvc@outlook.com>
2017-10-24 17:36:51 -0300

    lseek implementada

--- syscall.min.c
+++ syscall.min.c
@@ -1,4 +1,4 @@
-//#include <stdio.h>
+#include <stdio.h>
 //#include <string.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -15,24 +15,25 @@ int main() {
   char write_2[70] = "Digite o nome do arquivo que receberá o conteúdo copiado: ";
 
   write(1, write_1, 50);
-  read(0, input_file_name, 50);
+  read(0, input_file_name, sizeof(input_file_name));
 
   write(1, write_2, 70);
-  read(0, output_file_name, 50);
+  read(0, output_file_name, sizeof(output_file_name));
 
-  int fd = open(input_file_name, O_RDWR | O_CREAT, S_IWRITE | S_IREAD);
-  int fd2 = open(output_file_name, O_RDWR | O_CREAT, S_IWRITE | S_IREAD);
+  int fd = open(input_file_name, O_RDWR | O_CREAT, 777);
+  int fd2 = open(output_file_name, O_RDWR | O_CREAT, 777);
 
   if (fd == -1 || fd2 == -1){
     write(0, "Failed to open.\n", 15);
     exit(1);
   }
 
-  read(fd, fdd, 50);
-  write(*fdd, &fd, 50);
 
-  write(fd2, fdd, 50);
-  write(1, fdd, 50);
+  read(fd, fdd, sizeof(fd));
+  int size = lseek(fd, 0, SEEK_END);
+  write(*fdd, &fd, size);
+
+  write(fd2, fdd, size);
 
   close(fd);
   close(fd2);
